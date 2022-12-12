e36134c3014f1ddab5217c4113f3d4bb9781eaba
Ardiyan Agus Prayoga <iyan@sgo.co.id>
2022-12-12 10:37:41 +0700

    add migration and seeders

---
+++ migrations/20221208105850-create-users.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING(60),
+      },
+      password: {
+        type: Sequelize.STRING(60),
+      },
+      roles: {
+        type: Sequelize.STRING(30),
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING(128),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
