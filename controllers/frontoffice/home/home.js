b9d9f99197de287b45f75bd1e21ef0f15f14cac1
Ardiyan Agus Prayoga <iyan@sgo.co.id>
2022-12-25 01:09:12 +0700

    update home front office

--- controllers/frontoffice/home/home.js
+++ controllers/frontoffice/home/home.js
@@ -1,12 +1,64 @@
 const Book = require("../../../models/backoffice/books/book");
+const encrypted = require("../../../util/encrypted");
 
 exports.getHome = (req, res, next) => {
-  Book.findAll()
+  Book.findAll({
+    raw: true,
+  })
     .then((books) => {
+      let results = [];
+      let index = 0;
+      books.forEach((book) => {
+        if (results.length == 0) {
+          results.push({
+            category: book.category,
+            book: [
+              {
+                id: book.id,
+                title: book.title,
+                author: book.author,
+                description: book.description,
+                image: book.image,
+                createdAt: book.createdAt,
+              },
+            ],
+          });
+        } else {
+          if (book.category == results[index].category) {
+            results[index].book.push({
+              id: book.id,
+              title: book.title,
+              author: book.author,
+              description: book.description,
+              image: book.image,
+              createdAt: book.createdAt,
+            });
+          } else if (!book.category.includes(results[index].category)) {
+            results.push({
+              category: book.category,
+              book: [
+                {
+                  id: book.id,
+                  title: book.title,
+                  author: book.author,
+                  description: book.description,
+                  image: book.image,
+                  createdAt: book.createdAt,
+                },
+              ],
+            });
+            index++;
+          }
+        }
+      });
       res.render("frontoffice/home/index", {
-        books,
+        results,
+        encrypt: encrypted.encrypt,
       });
     })
     .catch((err) => console.log(err));
-  //   res.render("frontoffice/home/index");
+};
+
+exports.getBookByCategories = (req, res, next) => {
+  res.render("frontoffice/home/categories");
 };
--- routes/frontoffice/home/home.js
+++ routes/frontoffice/home/home.js
@@ -5,4 +5,6 @@ const homeController = require("../../../controllers/frontoffice/home/home");
 
 router.get("/", homeController.getHome);
 
+router.get('/categories', homeController.getBookByCategories);
+
 module.exports = { router };
