43d1cc5b2d8d24e71f89268187fffc4cc49d310c
ardiyan15 <ardhiyan15@gmail.com>
2024-11-27 20:43:18 +0700

    delete unused code

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,19 @@
 import 'module-alias/register';
-import fs from "fs";
 import path from "path";
 
-import express, { RequestHandler } from "express";
+import express from "express";
 import session from "express-session";
-import multer from "multer";
 import flash from "connect-flash";
 
 import sequelize from "@utils/connection";
-import startConsumer from '@utils/consumer';
 
 // Backoffice
 import backHomeRoutes from "./routes/backoffice/home/home";
 import bookRoutes from "./routes/backoffice/books/index";
 import userRoutes from "@routes/backoffice/users/index";
 
-// FrontOffice
-// import homeRoutes from "./routes/frontoffice/home/home";
 import Book from "./models/backoffice/books/book";
 import Rating from "./models/frontoffice/rating";
-
-interface MulterRequest extends Request {
-  file: any;
-}
 
 const app = express();
 
@@ -36,41 +27,7 @@ app.use(
 
 app.use(express.urlencoded({ extended: false }));
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const { destination } = req.body;
-    const userDir = `./images/${destination}`;
-    if (!fs.existsSync(userDir)) {
-      fs.mkdirSync(userDir, { recursive: true });
-    }
-    if (destination != null) {
-      cb(null, `images/${destination}`);
-    } else {
-      cb(null, "images");
-    }
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
-    );
-  },
-});
-
-// const fileFilter: RequestHandler = (req, file, next) => {
-//   if(file) {
-//     if(file.mimetype === "image/png")
-//   }
-// }
-
 app.use(flash());
-// app.use(express.urlencoded({ extended: false }))
-// app.use(
-//   multer({
-//     storage: fileStorage,
-
-//   })
-// )
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -83,17 +40,11 @@ app.use("/backoffice", backHomeRoutes);
 app.use("/backoffice", bookRoutes);
 app.use("/backoffice", userRoutes)
 
-// Frontoffice
-// app.use("/", homeRoutes);
-
 Book.hasMany(Rating, {
   foreignKey: "bookId",
 });
 
 Rating.belongsTo(Book);
-
-// startConsumer()
-// startConsumer().catch(console.error)
 
 sequelize
   .sync()
